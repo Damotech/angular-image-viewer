6a7ad755733f8287bc7a541e91f3774389555541
Maxime Carriere <maximecarriere@damotech.com>
2022-06-28 12:29:49 -0400

    Remove both browser modules to avoid conflicts when this lib is used in a lazy loaded module.

--- projects/angular-image-viewer/src/lib/angular-image-viewer.module.ts
+++ projects/angular-image-viewer/src/lib/angular-image-viewer.module.ts
@@ -1,38 +1,12 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularImageViewerComponent } from './angular-image-viewer.component';
-import { FullScreenDirective } from './directives/full-screen.directive';
-// import { ImageViewerConfig } from 'dist/angular-image-viewer/public-api';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { AngularImageViewerComponent } from "./angular-image-viewer.component";
+import { FullScreenDirective } from "./directives/full-screen.directive";
 
 @NgModule({
   declarations: [AngularImageViewerComponent, FullScreenDirective],
-  imports: [
-    CommonModule,
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-  ],
-  exports: [AngularImageViewerComponent, FullScreenDirective]
+  imports: [CommonModule, FormsModule],
+  exports: [AngularImageViewerComponent, FullScreenDirective],
 })
-export class AngularImageViewerModule {
-  /**
-   * forRoot
-   * @returns A module with its provider dependencies
-   */
-  // static forRoot(imageViewerConfig: ImageViewerConfig): ModuleWithProviders {
-  //   return {
-  //     ngModule: AngularImageViewerModule,
-  //     providers: [
-  //       {
-  //         provide: imageViewerConfig,
-  //         useValue: imageViewerConfig
-  //       }
-  //     ]
-  //   };
-  // }
-}
+export class AngularImageViewerModule {}
